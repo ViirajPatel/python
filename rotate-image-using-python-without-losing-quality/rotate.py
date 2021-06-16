d5c78e2734008717e62ce2569d9560d5fe722927
ViirajPatel <77567895+ViirajPatel@users.noreply.github.com>
2021-06-16 10:26:21 +0530

    Create rotate.py

---
+++ rotate-image-using-python-without-losing-quality/rotate.py
@@ -0,0 +1,16 @@
+#install opencv if you haven't install using py -m pip install opencv
+import cv2
+# import os utilities
+import os
+# rotatecodes
+# ROTATE_90_CLOCKWISE , ROTATE_90_COUNTERCLOCKWISE , ROTATE_180
+for image in os.listdir(os.getcwd()):
+    if(image == "rotate.py" or image=="output"): #here i deducted the name of other files and folders
+        continue
+    else:
+        img = cv2.imread(image)
+        # rotate and save the image with the same filename
+        rotated_img = cv2.rotate(img, rotateCode=cv2.ROTATE_90_COUNTERCLOCKWISE) 
+        cv2.imwrite("output\\"+image, rotated_img)
+       
+
